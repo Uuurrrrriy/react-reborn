60f3640d6d88a948c007c5787bcae958ed60edeb
Uuurrrrriy / <ihnatchukud@gmail.com>
2020-07-15 20:07:48 +0300

    lesson 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// const Button = ({value,isDisabled,onClick})=> {
+//
+//     const innerClickHandler = ()=>{
+//       onClick('some data from child button')
+//     };
+//
+//     return (
+//         <div onClick={innerClickHandler} className={`btnClass ${isDisabled && 'disabled'}`}>
+//             {value}
+//         </div>
+//     )
+// };
+
+// value
+// onClickHandler
+// is disabled
+// classname
+
+const Switcher = ({onClick})=>{
+    const [isOn,setIsOn] = useState(false);
+
+    const toggleSwitcher = () => {
+        setIsOn(!isOn)
+    };
+
+    return (
+      <div className={`switcher ${ isOn ? 'isOn' : 'isOff' }`} onClick={toggleSwitcher}>
+        <div className={`tumbler`}/>
+      </div>
+  )
+};
+
 function App() {
+    // const [val,setVal] = useState('');
+    //
+    // const onClickFromParent = data=> console.log(data, 'called in app');
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Switcher/>
+        {/*<input type='text' value={val}  onChange={ ({target: {value}})=>setVal(value) }/>*/}
+         {/*//  <Button value='345' isDisabled={val.length < 10} onClick={onClickFromParent}/>*/}
     </div>
   );
 }
